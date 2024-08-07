9b9cda0949819a1cc56a4cc5a8144ca3343d11d4
Алексей Рассказов <lesharasskazov4@gmail.com>
2024-08-08 01:36:34 +0500

    chore: added logic to calculate change, if successful, then issue change, test works

--- ChangeTests/Change.cs
+++ ChangeTests/Change.cs
@@ -39,7 +39,33 @@ namespace ChangeTests
             List<int> changeCoins = new List<int>();
             Dictionary<int, int> res = new Dictionary<int, int>();
 
-            
+            // Add coins to the change list and subtract their value from the amount
+            foreach (int coin in coinDenominations)
+            {
+                // We calculate the maximum number of coins of the current denomination that can be used
+                int maxCoins = Math.Min(availableCoins[coin], amount / coin);
+
+                // Add coins to the change list and subtract their value from the amount
+                for (int i = 0; i < maxCoins; i++)
+                {
+                    if (res.ContainsKey(coin))
+                    {
+                        res[coin]++;
+                    }
+                    else
+                    {
+                        res.Add(coin, 1);
+                    }
+
+                    amount -= coin;
+                }
+
+                // If the amount is 0, we have successfully calculated the change.
+                if (amount == 0)
+                {
+                    return (true, res);
+                }
+            }
 
 
 
