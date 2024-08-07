ffb15cd188c6f7fa716a691d2e7b12cfaa27006d
Алексей Рассказов <lesharasskazov4@gmail.com>
2024-08-08 00:58:51 +0500

    test: initialized set up for optimal testing

--- ChangeTests/ChangeUnitTests.cs
+++ ChangeTests/ChangeUnitTests.cs
@@ -3,6 +3,26 @@ namespace ChangeTests
     [TestClass]
     public class ChangeUnitTests
     {
+        private Dictionary<int, int> availableCoins;
+        MyCash myCash;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            availableCoins = new Dictionary<int, int> {
+            { 100, 10 },
+            { 50, 20 },
+            { 10, 50 },
+            { 5, 100 },
+            { 2, 100 },
+            { 1, 100 }
+            };
+
+            myCash = new MyCash(availableCoins);
+        }
+
+
         [TestMethod]
         public void TestMethod1()
         {
